8808bd79cc0dc52f5c5e9b6a969f89c06e4bd971
Fernando Brum <dev.fobrum@gmail.com>
2024-06-15 11:31:01 -0300

    Remove dist files (ops)

--- packages/backend/dist/instance.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize("mssql://sa:My0wnStr0ngP4ssItsS3cr3t@127.0.0.1:1433/TestDB");
-exports.default = sequelize;
